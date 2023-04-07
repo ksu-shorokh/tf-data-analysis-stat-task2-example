9b6f341205565e5dd7edcf693924f855c452fe03
ksu-shorokh <91955471+ksu-shorokh@users.noreply.github.com>
2023-04-07 22:37:24 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,5 +12,5 @@ def solution(p: float, x: np.array) -> tuple:
     #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
     #return loc - scale * norm.ppf(1 - alpha / 2), \
     #       loc - scale * norm.ppf(alpha / 2)
-    return (2 / (32 * 32)) * (expon.ppf(1 - alpha / 2) - 1/2) / loc, (2 / (32 * 32)) * (expon.ppf(alpha / 2) - 1/2) / loc
+    return (2 / (32 * 32)) * (- expon.ppf(1 - alpha / 2) + 1/2) / loc, (2 / (32 * 32)) * (- expon.ppf(alpha / 2) + 1/2) / loc
 
