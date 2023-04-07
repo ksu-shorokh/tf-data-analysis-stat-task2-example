f6124fd30f84c3f95f7c0cab31c02ef429bf9885
ksu-shorokh <91955471+ksu-shorokh@users.noreply.github.com>
2023-04-07 22:42:28 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.stats import expon
 
 
-chat_id = 417658432 
+chat_id = 845786312
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
@@ -12,5 +12,5 @@ def solution(p: float, x: np.array) -> tuple:
     #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
     #return loc - scale * norm.ppf(1 - alpha / 2), \
     #       loc - scale * norm.ppf(alpha / 2)
-    return (2 / (32 * 32)) * (- expon.ppf(1 - alpha / 2) + 1/2) / loc, (2 / (32 * 32)) * (- expon.ppf(alpha / 2) + 1/2) / loc
+    return (2 / (44 * 44)) * (- expon.ppf(1 - alpha / 2) + 1/2) / loc, (2 / (44 * 44)) * (- expon.ppf(alpha / 2) + 1/2) / loc
 
